d2516acf76c50c40a7a1ab4893b4a61d6e39ff32
sardar hasnain panhwer <hasnainpanhwer1@gmail.com>
2025-09-14 10:29:22 +0000

    Your CSV file should have the following columns. The order of columns is

--- src/app/dashboard/scanning/page.tsx
+++ src/app/dashboard/scanning/page.tsx
@@ -341,10 +341,7 @@ export default function ScanningPage() {
                     <Upload className="mr-2 h-4 w-4" />
                     Import CSV
                 </Button>
-                <CardDescription className="mt-4">
-                  Your CSV file should have the following columns. The order of columns is important.
-                </CardDescription>
-                 <CardDescription className="mt-2 font-sindhi text-lg">
+                <CardDescription className="mt-2 font-sindhi text-lg">
                   توهان جي. CSV فائل ۾ هيٺيان ڪالمن هجڻ گهرجن. ڪالمن جي ترتيب اهم آهي.
                 </CardDescription>
                 <div className="overflow-x-auto mt-2">
