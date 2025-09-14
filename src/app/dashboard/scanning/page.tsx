ea076bcb771b55a7f99c6156bbaaf6da390feb2d
sardar hasnain panhwer <hasnainpanhwer1@gmail.com>
2025-09-14 10:33:38 +0000

    توهان جي فائل ۾ هيٺيان ڪالمن هجڻ گهرجن، ڪالمن جي ترتيب اهم آهي۔ ya kado

--- src/app/dashboard/scanning/page.tsx
+++ src/app/dashboard/scanning/page.tsx
@@ -345,7 +345,7 @@ export default function ScanningPage() {
                   Your CSV file should have the following columns. The order of columns is important.
                 </CardDescription>
                 <CardDescription className="mt-2 font-sindhi text-lg">
-                  توهان جي. CSV فائل ۾ هيٺيان ڪالمن هجڻ گهرجن. ڪالمن جي ترتيب اهم آهي.
+                  توهان جي فائل ۾ هيٺيان ڪالمن هجڻ گهرجن، ڪالمن جي ترتيب اهم آهي۔
                 </CardDescription>
                 <div className="overflow-x-auto mt-2">
                     <Table>
